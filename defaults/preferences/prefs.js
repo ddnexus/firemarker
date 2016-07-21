3af1eab6b11b8be137db6892902a13270e13611e
Domizio Demichelis <dd.nexus@gmail.com>
2016-07-21 14:54:13 +0700

    version 0.0.13; added xpi file

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,2 +1,3 @@
 pref("extensions.firebug.firemarker.namespaces", "info erb dryml i18n");
 pref("extensions.firebug.firemarker.activeNamespace", "dryml");
+pref("extensions.firebug.DBG_FIREMARKER", false);
